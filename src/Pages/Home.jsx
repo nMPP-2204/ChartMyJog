4171dd7a77435c43a951e426439a06ec4f808bc7
Alexander Tikhomirov <alexander_a_tikhomirov@yahoo.com>
2022-07-01 15:54:09 -0700

    added proof of concept files in a folder, installed react router dom and made Home and runtracker pages

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,16 @@
+import {Link} from 'react-router-dom'
+
+function Home() {
+    return (
+      <div className='Home'>
+        <header>
+          <p className="pageHeader">Home</p>
+        </header>
+        <main>
+
+        </main>
+      </div>
+    );
+  }
+  
+  export default Home
