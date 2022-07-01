97fbe3fb43ae46d3284ce9aa00ffdf87b65e2f93
Andre Barber <andre.barber0505@gmail.com>
2022-07-01 18:51:05 -0500

    firebase install

---
+++ src/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBqIwWR4VO3HQ36_Fo-_nKKKqFnuSJDzJk",
+  authDomain: "chartmyjog-8a62d.firebaseapp.com",
+  projectId: "chartmyjog-8a62d",
+  storageBucket: "chartmyjog-8a62d.appspot.com",
+  messagingSenderId: "103578850787",
+  appId: "1:103578850787:web:44a8521f1243be2b51fcd6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
