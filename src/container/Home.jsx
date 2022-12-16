fa549bc671a68d39a305459f1031015b1e0121b5
Kevin Yu <Kevin.Yu.2468@gmail.com>
2022-12-16 12:55:53 -0800

    Update styling for home page with scss and react-responsive

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,32 +1,45 @@
-import { Typography } from "@mui/material";
+import "../App.scss";
 import Footer from "../component/Footer";
-// import RunningPeople from "../Images/runningPeople.jpg";
-import RunningWoman from "../Images/runningwoman.jpg";
 import Carousel from "../component/Carousel";
-import SignUpLogin from "./SignUpLogin";
 import Background from "../Images/Background.gif";
+import { Link } from "react-router-dom";
 
 const Home = () => {
+  const styledLink = { color: "black", textDecoration: "none" };
+
   return (
     <div className="Home">
       <header></header>
       <main>
-        <div style={{ backgroundImage: `url(${Background})` }}>
-          <div
-            style={{
-              display: "flex",
-              justifyContent: "center",
-            }}
-          >
-            <Typography
-              fontWeight={700}
-              sx={{ fontSize: { lg: "34px", xs: "30px" } }}
-              mb="23px"
-              mt="30px"
-            >
-              When your legs get tired <br />
-              Run with your heart ...
-            </Typography>
+        <div
+          style={{
+            backgroundImage: `url(${Background})`,
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "start",
+            alignItems: "center",
+          }}
+        >
+          <div className="text-white text-center text-4xl font-serif py-4 w-9/12">
+            Explore the world one step at a time
+            <br />
+            <p className="text-xl py-3">
+              Many runners around the world use{" "}
+              <span className="italic text-3xl">Chart My Jog</span> to track
+              their jogs and showcase
+              <br />
+              <br />
+              <span className="text-4xl animate-charcter">
+                achievements {"&"} improvements
+              </span>
+            </p>
+          </div>
+          <div className="start-run mb-5">
+            <Link to="/signup" style={styledLink}>
+              <button>
+                <span className="font-serif">Start RUN</span>
+              </button>
+            </Link>
           </div>
           <Carousel />
         </div>
