57a5f4bc839f2b7a4d4abf0185de121856f49dbc
Kevin Yu <Kevin.Yu.2468@gmail.com>
2023-01-03 08:15:31 -0800

    Add background gif and styled signup box with react icons

--- src/component/Video/VideoPlayer.jsx
+++ src/component/Video/VideoPlayer.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactPlayer from "react-player/lazy";
 
-const VideoPlayer = ({ src, ...args }) => {
-  console.log(src, args);
-  return <ReactPlayer url={src} {...args} />;
+const VideoPlayer = ({ video, ...args }) => {
+  const publicURL = "/video/";
+  const fullURL = publicURL + video;
+
+  return <ReactPlayer url={fullURL} {...args} />;
 };
 
 export default VideoPlayer;
--- src/container/SignUpLogin.jsx
+++ src/container/SignUpLogin.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+
 import * as firebaseui from "firebaseui";
 import "firebaseui/dist/firebaseui.css";
 import { auth } from "../utils/firebase.js";
@@ -11,7 +13,12 @@ import {
 } from "firebase/auth";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { createUser, getUser } from "../utils/firestore.js";
-import Background from "../assets/img/Background.gif";
+
+import background from "../assets/video/shoe-walking-background.mp4";
+import Loader from "../component/Loader/Loader.js";
+
+import { FiLogOut } from "react-icons/fi";
+import { FaRunning } from "react-icons/fa";
 
 const SignUpLogin = () => {
   const [user, loading, error] = useAuthState(auth);
@@ -20,37 +27,25 @@ const SignUpLogin = () => {
     signOut(auth);
   };
 
-  const uiConfig = {
-    signInSuccessUrl: "/",
-    signInOptions: [
-      EmailAuthProvider.PROVIDER_ID,
-      GoogleAuthProvider.PROVIDER_ID,
-      GithubAuthProvider.PROVIDER_ID,
-      FacebookAuthProvider.PROVIDER_ID,
-    ],
-    tosUrl: "https://chartmyjog-8a62d.web.app/dashboard",
-    privacyPolicyUrl: function () {
-      window.location.assign("https://chartmyjog-8a62d.web.app/home");
-    },
-  };
-
   useEffect(() => {
     const ui =
       firebaseui.auth.AuthUI.getInstance() || new firebaseui.auth.AuthUI(auth);
     if (!user) ui.start("#firebaseui-auth-container", uiConfig);
-  }, [user]);
+  }, [user, loading]);
 
-  // if (loading) {
-  //   return (
-  //     <div id="firebaseui-auth-container">
-  //       <p>Initialising User...</p>
-  //     </div>
-  //   );
-  // }
   if (error) {
     return (
       <div id="firebaseui-auth-container">
         <p>Error: {error}</p>
+      </div>
+    );
+  }
+
+  if (loading) {
+    return (
+      <div id="firebaseui-auth-container">
+        <p>Initialising User...</p>
+        <Loader />
       </div>
     );
   }
@@ -64,40 +59,96 @@ const SignUpLogin = () => {
     }
 
     return (
-      <div className="signout">
-        <div id="firebaseui-auth-container">
-          <p>Current User: {user.email}</p>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <button className="startRun" onClick={logout}>
-              Log out
-            </button>
-          </div>
-        </div>
-      </div>
+      <>
+        <SignOutBox>
+          <UserInfo user={user} />
+          <SignOutBtn logout={logout} />
+          <StartRunBtn />
+        </SignOutBox>
+        <Background />
+      </>
     );
   }
 
   return (
-    <div
-      style={{
-        backgroundImage: `url(${Background})`,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "start",
-        alignItems: "center",
-      }}
-      className="h-screen"
-    >
-      <div className="my-6 text-white text-3xl flex flex-col text-center font-serif">
-        <p className="my-4">Sign up or Sign in to </p>
-        <p className="text-4xl italic animate-charcter">Chart My Jog</p>
-      </div>
-      <div id="firebaseui-auth-container"></div>
-      {/* <div className="signin-gif"> */}
+    <>
+      <SignUpBox />
+      <Background />
+    </>
+  );
+};
 
-      {/* </div> */}
+const UserInfo = ({ user }) => {
+  return (
+    <>
+      <p className="mt-4">Welcome, </p>
+      <strong className="my-4 italic text-4xl">{user.email}</strong>
+    </>
+  );
+};
+
+const SignOutBox = ({ children }) => {
+  return (
+    <div className="my-16 px-8 py-10 bg-slate-200 text-3xl w-80 flex flex-col mx-auto rounded-lg text-center font-serif dark:text-white">
+      {children}
     </div>
   );
+};
+
+const SignOutBtn = ({ logout }) => {
+  return (
+    <button className={"mb-4 bg-red-500 " + buttonStyle} onClick={logout}>
+      <FiLogOut className="h-full ml-4" />
+      <p className="h-full ml-6">Log out</p>
+    </button>
+  );
+};
+
+const StartRunBtn = () => {
+  const styledLink = { textDecoration: "none" };
+  return (
+    <Link to="/run-tracker" style={styledLink}>
+      <button className={"bg-green-500 " + buttonStyle}>
+        <FaRunning className="h-full ml-4" />
+        <p className="h-full ml-6 no-underline">Start Run</p>
+      </button>
+    </Link>
+  );
+};
+
+const SignUpBox = () => {
+  return (
+    <div className="my-16 bg-white text-3xl w-80 flex flex-col mx-auto rounded-lg text-center font-serif dark:text-white">
+      <p className="my-4">Sign up or Sign in to </p>
+      <p className="text-4xl italic mb-4">Chart My Jog</p>
+      <div id="firebaseui-auth-container"></div>
+    </div>
+  );
+};
+
+const Background = () => {
+  return (
+    <video className="w-screen fixed top-0 left-0 -z-10" autoPlay loop muted>
+      <source src={background} type="video/mp4" />
+    </video>
+  );
+};
+
+const buttonStyle =
+  "z-10 rounded-full border-black w-64 h-20 p-3 text-4xl font-serif text-white flex no-wrap content-center";
+
+const uiConfig = {
+  signInSuccessUrl: "/signup",
+  signInOptions: [
+    EmailAuthProvider.PROVIDER_ID,
+    GoogleAuthProvider.PROVIDER_ID,
+    GithubAuthProvider.PROVIDER_ID,
+    FacebookAuthProvider.PROVIDER_ID,
+  ],
+  tosUrl: "https://chartmyjog-8a62d.web.app/dashboard",
+  privacyPolicyUrl: function () {
+    window.location.assign("https://chartmyjog-8a62d.web.app/home");
+  },
 };
 
 export default SignUpLogin;
