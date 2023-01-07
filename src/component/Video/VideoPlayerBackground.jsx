bd8c760dda6b348b325b2efaed5a5c09c2b4c581
Kevin Yu <Kevin.Yu.2468@gmail.com>
2023-01-06 21:50:45 -0800

    Revert commit status to original post-grad status

---
+++ src/component/Video/VideoPlayerBackground.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const VideoPlayerBackground = ({ video, tailwindStyle, ...args }) => {
+  const publicURL = "/video/";
+  const fullURL = publicURL + video;
+  const videoStyle = `hidden md:block w-screen absolute top-6 left-0 -z-10 ${
+    tailwindStyle || ""
+  }`;
+
+  return (
+    <>
+      <video
+        id="background"
+        className={videoStyle}
+        autoPlay
+        loop
+        muted
+        {...args}
+      >
+        <source src={fullURL} type="video/mp4" />
+      </video>
+    </>
+  );
+};
+
+export default VideoPlayerBackground;
